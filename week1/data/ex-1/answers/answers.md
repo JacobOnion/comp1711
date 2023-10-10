b483ef94a806efb0fb8068a9aae28b53c5fd2253
Jacob Onion <95771821+JacobOnion@users.noreply.github.com>
2023-10-10 08:30:33 +0000

    hwk?

---
+++ week1/data/ex-1/answers/answers.md
@@ -0,0 +1,5 @@
+Exercise 2
+#1: cat birds.txt | cut -d "," -f  2 | sort
+
+Exercise 3
+#1: cat *.txt | sort | head -n 50 | tail -n 1
--- week2/worksheet_1.c
+++ week2/worksheet_1.c
@@ -1,4 +1,40 @@
 #include<stdio.h>
+#include<limits.h>
+#include<float.h>
 int main(){
-    
+    //ex3();
+    //ex6();
+    ex8();
+    return 0;
+}
+
+void ex3(){
+    printf(" %d\n %d\n %d\n %d\n %f\n %f\n %f\n %f\n", CHAR_MIN, CHAR_MAX, INT_MIN, INT_MAX, FLT_MIN, FLT_MAX, DBL_MIN, DBL_MAX);
+}
+
+void ex6(){
+    int out;
+    printf("Input an integer: ");
+    scanf("%d", &out);
+    if(out % 2 == 1)
+    {
+        printf("odd\n");
+    }
+    else
+    {
+        printf("even\n");
+    }
+}
+
+void ex8(){
+    int num;
+    int total = 1;
+    printf("Enter an integer: ");
+    scanf("%d", num);
+    for (int i = 1; i <= num; i++)
+    {
+        printf("%d", i);
+        total *= i;
+    }
+    printf("%d\n", num);
 }
