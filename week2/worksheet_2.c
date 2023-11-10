382997c0bc3207a210dc68362d2e000c4b477d7e
Jacob Onion <95771821+JacobOnion@users.noreply.github.com>
2023-11-10 13:38:43 +0000

    shut down time :)

--- week2/worksheet_2.c
+++ week2/worksheet_2.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main(){
     //ex2_int();
-    ex4();
+    //ex4();
+    //ex5();
+    ex6();
     return 0;
 }
 
@@ -39,24 +41,66 @@ void ex2_int(){
 
 void ex4(){
     int numList[6] = {1, 2, 3, 4, 5};
+    int shiftedList[6];
     int temp1 = numList[0];
-    int temp2;
     int array_size = sizeof(numList) / sizeof(numList[0]);
     for(int i = 0; i < array_size-1; i++)
     {
         if(i == array_size-2)
         {
-            numList[0] = numList[i];
+            shiftedList[0] = numList[i];
         }
         else
         {
-            numList[i+1] = temp1;
-            temp2 = numList[i+1];
+            shiftedList[i+1] = numList[i];
         }
     }
     printf("the shifted array is:\n");
     for (int j = 0; j < array_size-1; j++)
     {
-        printf("%d\n", numList[j]);
+        printf("%d\n", shiftedList[j]);
+    }
+}
+
+void ex5()
+{
+    int numList[7] = {3, 56, 56, 8, 3, 32};
+    int dupes[7];
+    int dupeCount = 0;
+    int list_len = sizeof(numList) / sizeof(numList[0]);
+    for (int i = 0; i < list_len - 1; i++)
+    {
+        for (int j = 0; j < list_len - 1; j++)
+        {
+            if (i != j & numList[i] == numList[j] & dupes[i] != i & dupes[j] != j)
+            {
+                dupes[dupeCount] = numList[i];
+                dupeCount +=1;
+                printf("%d\n", numList[i]);
+                dupes[j] = j;
+                dupes[i] = j;
+            }
+        }
+    }
+}
+
+void ex6(){
+    int array1[4] = {2, 56, 7};
+    int array2[4] = {45, 6, 98};
+    int newArray[7];
+    int len1 = sizeof(array1)/sizeof(array1[0]);
+    int len2 = sizeof(array2)/sizeof(array2[0]);
+    for (int i = 0; i < len1 - 1; i++)
+    {
+        newArray[i] = array1[i];
+    }
+    for (int j = len1 - 1; j < len1 + len2 - 2; j++)
+    {
+        newArray[j] = array2[j - len1 + 1];
+    }
+    printf("The new list is:\n");
+    for (int l = 0; l < len1 + len2 - 2; l++)
+    {
+        printf("%d\n", newArray[l]);
     }
 }
